05430d6c1f34f60c040ff79529043e41a18a0c07
Jianqiang Sun <j.sun@affrc.go.jp>
2021-06-25 14:04:29 +0900

    update scripts for calculation of bam stats.

--- quant_shells/fullseq/calc_nmappedreads.sh
+++ quant_shells/fullseq/calc_nmappedreads.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-#PBS --group=g-mlbi
-#PBS -q cq
-#PBS -l gpunum_job=0
-#PBS -l cpunum_job=1
-#PBS -l elapstim_req=4:00:00
-#PBS -b 1
-#PBS -m be
-#PBS -N NB_FUL_BAMSTATS
+#$ -S /bin/bash
+#$ -jc hostos_g1
+#$ -cwd
+#$ -N qslog_hb_full_bamstats
+#$ -mods l_hard h_rt 288:00:00
+
 
 
 PROJECT_DIR=~/projects/HuoberBrezel
 
 
-cd ~/projects/HuoberBrezel/data
-cd fullseq/bam
+cd ${PROJECT_DIR}/data/fullseq/bam
 
 log_fpath=nreads_mapped_stats.tsv
 if [ -f ${log_fpath} ]; then
@@ -22,15 +19,37 @@ fi
 
 for bam_fpath in `ls *.bam`; do
     echo ${bam_fpath} >> ${log_fpath}
-    samtools view -h -f 0x2 ${bam_fpath} | grep 'chr.A' | grep -c 'NH:i:1' >> ${log_fpath}
-    samtools view -h -f 0x2 ${bam_fpath} | grep 'chr.B' | grep -c 'NH:i:1' >> ${log_fpath}
-    samtools view -h -f 0x2 ${bam_fpath} | grep 'chr.D' | grep -c 'NH:i:1' >> ${log_fpath}
+    samtools view -h -f 0x2 ${bam_fpath} | grep 'Chr.A' | grep -c 'NH:i:1' >> ${log_fpath}
+    samtools view -h -f 0x2 ${bam_fpath} | grep 'Chr.B' | grep -c 'NH:i:1' >> ${log_fpath}
+    samtools view -h -f 0x2 ${bam_fpath} | grep 'Chr.D' | grep -c 'NH:i:1' >> ${log_fpath}
 done
 
 
 
-cd ~/projects/HuoberBrezel/data
-cd fullseq/bamstar
+
+
+
+
+cd ${PROJECT_DIR}/data/shortfullseq/bam
+
+log_fpath=nreads_mapped_stats.tsv
+if [ -f ${log_fpath} ]; then
+    rm ${log_fpath}
+fi
+
+for bam_fpath in `ls *.bam`; do
+    echo ${bam_fpath} >> ${log_fpath}
+    samtools view -h ${bam_fpath} | grep 'Chr.A' | grep 'NH:i:1' | wc -l >> ${log_fpath}
+    samtools view -h ${bam_fpath} | grep 'Chr.B' | grep 'NH:i:1' | wc -l >> ${log_fpath}
+    samtools view -h ${bam_fpath} | grep 'Chr.D' | grep 'NH:i:1' | wc -l >> ${log_fpath}
+done
+
+
+
+
+
+
+cd ${PROJECT_DIR}/data/fullseq/bamstar
 
 log_fpath=nreads_mapped_stats.tsv
 if [ -f ${log_fpath} ]; then
@@ -46,17 +65,17 @@ for bam_fpath in `ls *.bam`; do
     samtools view -@ 8 -c -q 255 -f 9 -F 4 tmpbam_cdka8klxn >> ${log_fpath}
 
     echo '-- A --' >> ${log_fpath}
-    grep 'chr.A' tmpbam_cdka8klxn > tmpbam_cdka8klxn.A
+    grep 'Chr.A' tmpbam_cdka8klxn > tmpbam_cdka8klxn.A
     samtools view -@ 8 -c -q 255 -f 3 tmpbam_cdka8klxn.A >> ${log_fpath}
     samtools view -@ 8 -c -q 255 -f 9 -F 4 tmpbam_cdka8klxn.A >> ${log_fpath}
 
     echo '-- B --' >> ${log_fpath}
-    grep 'chr.B' tmpbam_cdka8klxn > tmpbam_cdka8klxn.B
+    grep 'Chr.B' tmpbam_cdka8klxn > tmpbam_cdka8klxn.B
     samtools view -@ 8 -c -q 255 -f 3 tmpbam_cdka8klxn.B >> ${log_fpath}
     samtools view -@ 8 -c -q 255 -f 9 -F 4 tmpbam_cdka8klxn.B >> ${log_fpath}
 
     echo '-- D --' >> ${log_fpath}
-    grep 'chr.D' tmpbam_cdka8klxn > tmpbam_cdka8klxn.D
+    grep 'Chr.D' tmpbam_cdka8klxn > tmpbam_cdka8klxn.D
     samtools view -@ 8 -c -q 255 -f 3 tmpbam_cdka8klxn.D >> ${log_fpath}
     samtools view -@ 8 -c -q 255 -f 9 -F 4 tmpbam_cdka8klxn.D >> ${log_fpath}
 
@@ -68,16 +87,15 @@ done
 
 
 
-cd ~/projects/HuoberBrezel/data
-cd fullseq/eaglerc
+cd ${PROJECT_DIR}/data/fullseq/eaglerc
 
 log_fpath=nreads_mapped_stats.tsv
 if [ -f ${log_fpath} ]; then
     rm ${log_fpath}
 fi
 
-for bam_fpath in `ls *.bam`; do
-    echo ${bam_fpath} >> ${log_fpath}
+for list_dpath in `ls`; do
+    echo ${list_dpath} >> ${log_fpath}
     grep -c REF ${list_dpath}/${list_dpath}.ref.chrA.list >> ${log_fpath}
     grep -c REF ${list_dpath}/${list_dpath}.ref.chrB.list >> ${log_fpath}
     grep -c REF ${list_dpath}/${list_dpath}.ref.chrD.list >> ${log_fpath}
--- quant_shells/tagseq/README.md
+++ quant_shells/tagseq/README.md
@@ -28,84 +28,16 @@ qsub 21_dwsample_quant.sh
 
 ## summarizing of mapping statistics
 
-To count the number of mapped reads mapped on A, B, and D-subgenome
-from HISAT2 mapping results, use the `samtools` with the following options.
+Use `calc_nmappedreads.sh` instead of the following scripts.
+
 
 ```bash
-cd ~/projects/HuoberBrezel/data
-for dpath in `ls -d tagseq*`; do
-    cd ${dpath}/bam
-    
-    log_fpath=nreads_mapped_stats.tsv
-    if [ -f ${log_fpath} ]; then
-        rm ${log_fpath}
-    fi
-    
-    for bam_fpath in `ls *.bam`; do
-        echo ${bam_fpath} >> ${log_fpath}
-        samtools view -h ${bam_fpath} | grep 'chr.A' | grep 'NH:i:1' | wc -l >> ${log_fpath}
-        samtools view -h ${bam_fpath} | grep 'chr.B' | grep 'NH:i:1' | wc -l >> ${log_fpath}
-        samtools view -h ${bam_fpath} | grep 'chr.D' | grep 'NH:i:1' | wc -l >> ${log_fpath}
-    done
-    
-    cd -
-done
-```
-
-To summarize STAR results, use the following commands.
-
-```bash
-cd ~/projects/HuoberBrezel/data
-cd tagseq/bamstar
-
-log_fpath=nreads_mapped_stats.tsv
-if [ -f ${log_fpath} ]; then
-    rm ${log_fpath}
-fi
-    
-for bam_fpath in `ls *.bam`; do
-    echo ${bam_fpath} >> ${log_fpath}
-    samtools view -h -q 255 ${bam_fpath} | grep 'chr.A' | grep -v "@SQ" | wc -l >> ${log_fpath}
-    samtools view -h -q 255 ${bam_fpath} | grep 'chr.B' | grep -v "@SQ" | wc -l >> ${log_fpath}
-    samtools view -h -q 255 ${bam_fpath} | grep 'chr.D' | grep -v "@SQ" | wc -l >> ${log_fpath}
-done
+qsub calc_nmappedreads.sh
 ```
 
 
-To summarize EAGLERC results, use the following commands.
-
-```bash
-cd ~/projects/HuoberBrezel/data
-cd tagseq/eaglerc
-
-log_fpath=nreads_mapped_stats.tsv
-if [ -f ${log_fpath} ]; then
-    rm ${log_fpath}
-fi
-
-for list_dpath in `ls -d *`; do
-    echo ${list_dpath} >> ${log_fpath}
-    grep -v REVERSE ${list_dpath}/${list_dpath}.ref.chrA.list | grep -c REF >> ${log_fpath}
-    grep -v REVERSE ${list_dpath}/${list_dpath}.ref.chrB.list | grep -c REF >> ${log_fpath}
-    grep -v REVERSE ${list_dpath}/${list_dpath}.ref.chrD.list | grep -c REF >> ${log_fpath}
-done
 
 
-cd tagseq/eaglercngi
-
-log_fpath=nreads_mapped_stats.tsv
-if [ -f ${log_fpath} ]; then
-    rm ${log_fpath}
-fi
-
-for list_dpath in `ls -d *`; do
-    echo ${list_dpath} >> ${log_fpath}
-    grep -v REVERSE ${list_dpath}/${list_dpath}.ref.chrA.list | grep -c REF >> ${log_fpath}
-    grep -v REVERSE ${list_dpath}/${list_dpath}.ref.chrB.list | grep -c REF >> ${log_fpath}
-    grep -v REVERSE ${list_dpath}/${list_dpath}.ref.chrD.list | grep -c REF >> ${log_fpath}
-done
-
-```
 
 
 
